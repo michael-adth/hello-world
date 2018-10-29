9916ec33418cbee249fa73db188ee1c48c7217e7
Michael Kravchyna <michael@adtheorent.com>
2018-10-29 15:05:39 -0400

    module 2 added

---
+++ hello/test_goodbye.py
@@ -0,0 +1,21 @@
+import unittest
+
+from hello import hello
+
+
+class TestGoodbye(unittest.TestCase):
+    """sample test"""
+
+    def test_moon(self):
+        """sample test"""
+        self.assertEqual(hello('world'), 'hello world')
+
+    def test_sun(self):
+        """sample test with unicode"""
+        self.assertEqual(hello(u'world'), u'hello world')
+
+    def test_mars(self):
+        """sample test with failure"""
+        self.assertEqual(2+2, 5)
+    
+    
